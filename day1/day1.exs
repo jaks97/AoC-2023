72a3d8ef55f03db46c3c64d5dc1942d5da15341d
jaks97 <jkhalilsilva@gmail.com>
2023-12-03 00:08:07 -0300

    Day 1

---
+++ day1/day1.exs
@@ -0,0 +1,17 @@
+calibration_value = fn str ->
+  str
+    |> String.to_charlist
+    |> Enum.filter(&(&1 in ?0..?9))
+    |> Enum.map(&(&1 - ?0))
+    |> (&[Enum.at(&1, 0, ?0) * 10, Enum.at(&1, -1, ?0)]).()
+    |> Enum.sum
+end
+
+File.read("input.txt")
+  |> elem(1)
+  |> String.split("\n")
+  |> Enum.map(calibration_value)
+  |> Enum.sum
+  |> IO.puts
+
+#54697
